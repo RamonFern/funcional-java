1a945850d469a5aa09b8b0c02641636da1d613b6
Ramon Fernandes <ramonfernandes9@gmail.com>
2020-11-16 16:46:49 -0300

    Função de alta ordem com todas as operações

--- src/br/com/digitalinnovation/exercicio/FuncaoDeAltaOrdem.java
+++ src/br/com/digitalinnovation/exercicio/FuncaoDeAltaOrdem.java
@@ -3,8 +3,15 @@ package br.com.digitalinnovation.exercicio;
 public class FuncaoDeAltaOrdem {
 	
 	public static void main(String[] args) {
-		Calculo soma = (a, b) -> a + b;
-		System.out.println(executarOperacao(soma, 1, 2));
+		Calculo SOMA = (a, b) -> a + b;
+		Calculo SUB = (a, b) -> a - b;
+		Calculo DIV = (a, b) -> a / b;
+		Calculo MULT = (a, b) -> a * b;
+		
+		System.out.println(executarOperacao(SOMA, 1, 2));// 3
+		System.out.println(executarOperacao(SUB, 4, 2));// 2
+		System.out.println(executarOperacao(DIV, 6, 2));// 3
+		System.out.println(executarOperacao(MULT, 1, 2));// 2
 	}
 
 	private static int executarOperacao(Calculo calculo, int a, int b) {
@@ -13,6 +20,7 @@ public class FuncaoDeAltaOrdem {
 //FUNÇÃO DE ALTA ORDEM É UMA FUNÇÃO QUE RECEBE UMA OUTRA FUNÇÃO OU QUE RETORNA UMA FUNÇÃO
 }
 
+@FunctionalInterface
 interface Calculo{
 	public int calcular(int a, int b);
 }
